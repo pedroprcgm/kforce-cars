a54f148fbad7704fb8b50a2fbd2890119c35ea06
Pedro Guerreiro <pedro.prcgm@gmail.com>
2023-02-26 01:57:14 -0300

    Initial commit

---
+++ KForceCars/Pages/Cars/AddNewCar.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KForceCars.Pages.Cars;
+
+public class Create : PageModel
+{
+    public Create()
+    {
+    }
+    
+    public void OnGet()
+    {
+        
+    }
+}
